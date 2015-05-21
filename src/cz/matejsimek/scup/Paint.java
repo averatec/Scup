cf26131ca5394fa4d068706af897e0b1392d5669
Mateusz Badzinski <mateusz.badzinski@egm.pl>
2015-05-21 15:18:30 +0200

    Okno edycji screena wyśrodkowane

--- src/cz/matejsimek/scup/Paint.java
+++ src/cz/matejsimek/scup/Paint.java
@@ -76,6 +76,8 @@ public class Paint extends javax.swing.JFrame {
         pack();
 
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+        this.setLocation(dim.width/2-this.getSize().width/2, dim.height/2-this.getSize().height/2);
         setVisible(true);
         color.setForeground(colorChooser.getColor().getColor());
     }
